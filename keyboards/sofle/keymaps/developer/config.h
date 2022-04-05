3a0dcbc9957b807c83962597d5c6b9e907dd529e
Anh Le Tuan Tran <tranletuananh1992@gmail.com>
2022-04-05 15:29:24 +0700

    Update my own configuration

---
+++ keyboards/sofle/keymaps/developer/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+#undef TAPPING_TERM
+#define TAPPING_TERM 155
+
+#define TAPPING_TERM_PER_KEY
+#define COMBO_COUNT 1
+
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
+
