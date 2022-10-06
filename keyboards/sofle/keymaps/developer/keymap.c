23cceba39753c09024d81162cc4d2f8d6ee8d62f
Anh Le Tuan Tran <tranletuananh1992@gmail.com>
2022-10-06 19:14:59 +0700

    Define enter and space keys

--- keyboards/sofle/keymaps/developer/keymap.c
+++ keyboards/sofle/keymaps/developer/keymap.c
@@ -24,7 +24,8 @@
 #define HOME_L KC_L
 #define HOME_SCLN KC_SCLN
 
-#define HOME_ENTER KC_SWAP_ENTER
+#define M_SPACE KC_SWAP_ENTER
+#define M_ENTER KC_SWAP_SPACE
 
 bool is_alt_tab_active = false;
 uint16_t alt_tab_timer = 0;
@@ -80,11 +81,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
-  MT(MOD_LCTL, KC_ESC),   HOME_A,   HOME_S,    HOME_D,    HOME_F,    KC_G,                     KC_H,    HOME_J,    HOME_K,    HOME_L, HOME_SCLN,  KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MTASK,     KC_TGUK ,KC_N,    KC_M, KC_COMM,  KC_DOT, HOME_SLASH,  KC_RSFT,
-  KC_LGUI,  KC_LALT,KC_LCTRL, KC_SWAP_SPACE,        KC_LOWER,             KC_RAISE,  HOME_ENTER, KC_RCTRL, KC_RALT, KC_RGUI
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
+  MT(MOD_LCTL, KC_ESC),   HOME_A,   HOME_S,    HOME_D,    HOME_F,    KC_G,             KC_H,    HOME_J,    HOME_K,    HOME_L, HOME_SCLN,  KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,      KC_MTASK,           KC_TGUK, KC_N,    KC_M, KC_COMM,  KC_DOT, HOME_SLASH,  KC_RSFT,
+  KC_LGUI,  KC_LALT,KC_LCTRL, M_ENTER,        KC_LOWER,             KC_RAISE,  M_SPACE, KC_RCTRL, KC_RALT, KC_RGUI
 ),
 
 /*
@@ -104,11 +105,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_COLEMAK] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
-  KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_BSPC,
-  KC_ESC,   KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_N,    KC_E,    KC_I,    KC_O,  KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,      XXXXXXX,KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-                 KC_LGUI,KC_LALT,KC_LCTRL,KC_SWAP_SPACE, KC_LOWER,      KC_RAISE,  KC_SWAP_ENTER, KC_RCTRL, KC_RALT, KC_RGUI
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                                KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
+  KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_G,                                KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,  KC_BSPC,
+  KC_ESC,   KC_A,   KC_R,    KC_S,    KC_T,    KC_D,                                KC_H,    KC_N,    KC_E,    KC_I,    KC_O,  KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,     KC_MUTE,      XXXXXXX,     KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+                 KC_LGUI,KC_LALT,KC_LCTRL,M_ENTER, KC_LOWER,     KC_RAISE,  M_SPACE, KC_RCTRL, KC_RALT, KC_RGUI
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
