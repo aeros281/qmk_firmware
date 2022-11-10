67790c3aaa18ea64aad12d00a57ecc8fdfce9f2e
Anh Le Tuan Tran <tranletuananh1992@gmail.com>
2022-11-10 17:12:29 +0700

    Add my own setting.
    New keycaps are not shine-through :(, so depend on the LED of the keys
    to know which layer is on is not possible. This patch make the indicator
    LED to be used in full range, theorically it will support showing the
    layer number from 0-63 (six binary bit).
    
    Support for the indicator of Caps Lock and Numlock is removed, since I
    do not depend on them for my workflow.

---
+++ keyboards/moonlander/keymaps/developer/rules.mk
@@ -0,0 +1,11 @@
+# Set any rules.mk overrides for your specific keymap here.
+# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+AUDIO_ENABLE = no
+NO_USB_STARTUP_CHECK = yes
+TAP_DANCE_ENABLE = yes
+SPACE_CADET_ENABLE = no
+CAPS_WORD_ENABLE = yes
+
+SRC = matrix.c
